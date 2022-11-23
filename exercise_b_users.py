f23c8a9f5bf874e11ee7266854c925e41786c865
Jonathan Strandberg <jonstrandberg7@gmail.com>
2022-11-23 09:15:25 +0000

    fixed errors in B

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -63,7 +63,8 @@ print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
 print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
-print(users["Erik"][min"lottery_numbers)"])
+min_lottery_number = min(users["Erik"]["lottery_numbers"])
+print(min_lottery_number)
 # 6. Return an list of Avril's lottery numbers that are even
 
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
